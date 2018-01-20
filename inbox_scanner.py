18f898fc600d7f87843e62e0ee419ba0b4af4004
Joseph Distler <jdistler@utexas.edu>
2018-01-20 10:13:37 -0600

    Wrap logic in a try-catch

--- inbox_scanner.py
+++ inbox_scanner.py
@@ -52,42 +52,50 @@ class InboxScanner:
         if message_table.find_one(message_id=item.name):
             self.log.info('Already in db, ignore')
         else:
-            author = item.author.name.lower()
-            if author != "reddit" and author != "xrb4u" and author != "raiblocks_tipbot" and author != "giftxrb" \
-                    and author != "automoderator":
+            try:
+                author = item.author.name.lower()
+                if author != "reddit" and author != "xrb4u" and author != "raiblocks_tipbot" and author != "giftxrb" \
+                        and author != "automoderator":
 
-                redditor = self.reddit_client.redditor(item.author.name)
+                    redditor = self.reddit_client.redditor(item.author.name)
 
-                created = redditor.created_utc
+                    created = redditor.created_utc
 
-                age = (datetime.utcnow() - datetime.fromtimestamp(int(created))).days
+                    age = (datetime.utcnow() - datetime.fromtimestamp(int(created))).days
 
-                comment_karma = redditor.comment_karma
+                    comment_karma = redditor.comment_karma
 
-                if age >= 10 and comment_karma >= 20:
+                    if age >= 10 and comment_karma >= 20:
 
-                    self.log.info("Item is as follows:")
-                    self.log.info((vars(item)))
+                        self.log.info("Item is as follows:")
+                        self.log.info((vars(item)))
 
-                    self.log.info("Attribute - Item was comment: " + str(item.was_comment))
+                        self.log.info("Attribute - Item was comment: " + str(item.was_comment))
 
-                    # Only care about mentions for the giveaway bot
+                        # Only care about mentions for the giveaway bot
 
-                    if item.was_comment:
-                        self.log.info("Comment subject: " + str(item.subject))
-                        if item.subject == 'username mention':
-                            self.process_mention(item)
+                        if item.was_comment:
+                            self.log.info("Comment subject: " + str(item.subject))
+                            if item.subject == 'username mention':
+                                self.process_mention(item)
+                        else:
+                            reply_message = 'Please do not send PMs to this bot. The main TipBot, /u/RaiBlocks_TipBot,' + \
+                                            ' should be used for interaction via PM \n\nGo to the [wiki]' + \
+                                            '(https://np.reddit.com/r/RaiBlocks_tipbot/wiki/giveaway) for more info'
+                            item.reply(reply_message)
                     else:
-                        reply_message = 'Please do not send PMs to this bot. The main TipBot, /u/RaiBlocks_TipBot,' + \
-                                        ' should be used for interaction via PM \n\nGo to the [wiki]' + \
+                        reply_message = 'Sorry! I cannot gift an account less than 10 days old or with less than' \
+                                        ' 20 comment karma\n\n This is to prevent bots from exploiting the RaiBlocks ' \
+                                        'giveaway \n\nGo to the [wiki]' + \
                                         '(https://np.reddit.com/r/RaiBlocks_tipbot/wiki/giveaway) for more info'
                         item.reply(reply_message)
-                else:
-                    reply_message = 'Sorry! I cannot gift an account less than 10 days old or with less than' \
-                                    ' 20 comment karma\n\n This is to prevent bots from exploiting the RaiBlocks ' \
-                                    'giveaway \n\nGo to the [wiki]' + \
-                                    '(https://np.reddit.com/r/RaiBlocks_tipbot/wiki/giveaway) for more info'
-                    item.reply(reply_message)
+            except:
+                reply_message = 'An error came up, your request could not be processed\n\n' + \
+                                ' Paging /u/valentulus_menskr error id: ' + item.name + '\n\n'
+                item.reply(reply_message)
+                self.log.error("Unexpected error: " + str(sys.exc_info()[0]))
+                tb = traceback.format_exc()
+                self.log.error(tb)
 
             # Add message to database
             record = dict(user_id=item.author.name, message_id=item.name)
